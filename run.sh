5c4f06c256553788afba9eec12fd0e1eabf35081
Matthew Mattox <mmattox@support.tools>
2021-02-21 21:44:02 -0600

    Testing

--- run.sh
+++ run.sh
@@ -1,4 +1,64 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+rolling-reboot() {
+  export KUBECONFIG=./kube_config_cluster.yml
+  for node in `kubectl get nodes -o name | awk -F'/' '{print $2}'`
+  do
+    echo "Node: $node"
+    status=`kubectl get nodes "$node" | tail -n1 | awk '{print $2}'`
+    echo "Checking if node is ready..."
+    if [[ "$status" == "Ready" ]]
+    then
+      echo "Cordoning node..."
+      kubectl cordon "$node"
+      kubectl drain "$node" --ignore-daemonsets --delete-local-data --force --grace-period=900
+      echo "Sleeping..."
+      sleep 360
+      echo "Updating..."
+      ssh root@"$node" 'apt update -y && apt upgrade -y'
+      echo "Rebooting..."
+      ssh root@"$node" 'reboot'
+      echo "Sleeping..."
+      sleep 360
+      echo "Waiting for ping..."
+      while ! ping -c 1 $node
+      do
+        echo "Waiting..."
+        sleep 1
+      done
+      echo "Waiting for SSH..."
+      while ! ssh root@"$node" "uptime"
+      do
+        echo "Waiting..."
+        sleep 1
+      done
+      echo "Waiting for docker..."
+      while ! ssh root@"$node" "docker ps"
+      do
+        echo "Waiting..."
+        sleep 1
+      done
+      echo "Sleeping..."
+      sleep 360
+      echo "Waiting for node ready..."
+      while ! kubectl get nodes "$node" | tail -n1 | awk '{print $2}' | grep "Ready"
+      do
+        echo "Waiting..."
+        sleep 1
+      done
+      echo "Sleeping..."
+      sleep 360
+      echo "Uncordoning node..."
+      kubectl uncordon "$node"
+      echo "Sleeping..."
+      sleep 360
+      rke up
+    else
+     echo "Uncordoning node..."
+     kubectl uncordon "$node"
+    fi
+}
+
 
 echo "Setting up SSH key..."
 if [[ -z $SSH_KEY ]]
@@ -14,20 +74,26 @@ then
   exit 1
 fi
 
+if [[ ! -d ~/clusters/"$Cluster" ]]
+then
+  echo "Cluster folder is missing"
+  exit 2
+fi
+
+if [[ ! -f ~/clusters/cluster.yml ]]
+then
+  echo "cluster.yml is missing"
+  exit 3
+fi
+
 if [[ "$Action" == "cluster_up" ]]
 then
-  if [[ ! -d ~/clusters/"$Cluster" ]]
-  then
-    echo "Cluster folder is missing"
-    exit 2
-  else
-    cd ~/clusters/"$Cluster"
-    if [[ ! -f ./cluster.yml ]]
-    then
-      echo "cluster.yml is missing"
-      exit 3
-    else
-      rke up --config cluster.yml
-    fi
-  fi
+  cd ~/clusters/"$Cluster"
+  rke up --config cluster.yml
+elfi [[ "$Action" == "rolling_reboot" ]]
+then
+  rolling-reboot
+else
+  echo "Unknown Action"
+  exit 254
 fi
